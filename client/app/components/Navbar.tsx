79325575c880e9cc512e701b7787655c779a4f63
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-04-10 02:51:12 +0530

    fix edit user info bug

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,69 +1,85 @@
 "use client";
 
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/app/utils/cn";
 import Link from "next/link";
 import { ThemeSwitcher } from "../utils/ThemeSwitcher";
 import { HiOutlineUserCircle } from "react-icons/hi2";
 import CustomModal from "../utils/CustomModal";
-import Login from '../components/Auth/Login'
-import SignUp from '../components/Auth/SignUp'
-import Verification from '../components/Auth/Verification'
-import { useSelector } from 'react-redux'
+import Login from "../components/Auth/Login";
+import SignUp from "../components/Auth/SignUp";
+import Verification from "../components/Auth/Verification";
+import { useSelector } from "react-redux";
 import Image from "next/image";
-import avatar from '../../public/global/avatar.png'
+import avatar from "../../public/global/avatar.png";
 import { useSession } from "next-auth/react";
-import { useLogOutQuery, useSocailAuthMutation } from "@/redux/features/auth/authApi";
+import {
+  useLogOutQuery,
+  useSocailAuthMutation,
+} from "@/redux/features/auth/authApi";
 import { toast } from "react-hot-toast";
 
 type Props = {
   open: boolean;
   setOpen: (open: boolean) => void;
   activeItem: number;
-  route:string;
+  route: string;
   setRoute: (route: string) => void;
 };
 
-const Navbar = ({className, open, setOpen, setRoute ,activeItem, route}: Props & { className?: string }) => {
+const Navbar = ({
+  className,
+  open,
+  setOpen,
+  setRoute,
+  activeItem,
+  route,
+}: Props & { className?: string }) => {
   const [active, setActive] = useState<string | null>(null);
-  const {user} = useSelector((state:any) => state.auth) // getting logged in user data 
-  const {data} = useSession(); //for social authentication 
-  const [socialAuth, {isSuccess, error}] = useSocailAuthMutation();
+  const { user } = useSelector((state: any) => state.auth); // getting logged in user data
+  const { data } = useSession(); //for social authentication
+  const [socialAuth, { isSuccess, error }] = useSocailAuthMutation();
   const [logout, setLogout] = useState(false);
   const {} = useLogOutQuery(undefined, {
     skip: !logout ? true : false,
   });
 
   useEffect(() => {
-    if(!user){
-      if(data){
+    if (!user) {
+      if (data) {
         socialAuth({
           email: data?.user?.email,
           name: data?.user?.name,
-          avatar: data.user?.image
-        })
+          avatar: data.user?.image,
+        });
       }
     }
-    if(data === null  ){
-      if(isSuccess){
-        toast.success("Login Successfull")
+    if (data === null) {
+      if (isSuccess) {
+        toast.success("Login Successfull");
       }
     }
-    if(data === null){// when session is removed 
+    if (data === null) {
+      // when session is removed
       setLogout(true);
     }
-  }, [data,user])
+  }, [data, user]);
 
-  
   return (
-    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
       <Menu setActive={setActive}>
-        <Link href={'/'}>
-          <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
         </Link>
         <MenuItem setActive={setActive} active={active} item="Courses">
-        <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
             <ProductItem
               title="Mern Stack"
               href="https://algochurn.com"
@@ -89,16 +105,15 @@ const Navbar = ({className, open, setOpen, setRoute ,activeItem, route}: Props &
               description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
             />
           </div>
-
         </MenuItem>
-        <Link href={'/about'}>
+        <Link href={"/about"}>
           <MenuItem setActive={setActive} active={active} item="About">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/courses">FAQ</HoveredLink>
-            <HoveredLink href="/courses">Policy</HoveredLink>
-            <HoveredLink href="/courses">Our Team</HoveredLink>
-            <HoveredLink href="/courses">Contact Us</HoveredLink>
-          </div>
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/courses">FAQ</HoveredLink>
+              <HoveredLink href="/courses">Policy</HoveredLink>
+              <HoveredLink href="/courses">Our Team</HoveredLink>
+              <HoveredLink href="/courses">Contact Us</HoveredLink>
+            </div>
           </MenuItem>
         </Link>
         {/* <Link href={'/policy'}>
@@ -107,76 +122,67 @@ const Navbar = ({className, open, setOpen, setRoute ,activeItem, route}: Props &
         {/* <Link href={'/faq'}>
           <MenuItem setActive={setActive} active={active} item="FAQ"></MenuItem>
         </Link> */}
-        {
-          user ? (
-            <Link href={'/profile'}>
-              <Image 
-                src={user.avatar ? user.avatar : avatar}
-                alt=""
-                className="w-[30px] h-[30px] rounded-full cursor-pointer"
-              />
-            </Link>
-          ) : (
-           <HiOutlineUserCircle 
+        {user ? (
+          <Link href={"/profile"}>
+            <Image
+              src={user.avatar ? user.avatar.url : avatar}
+              alt=""
+              width={30}
+              height={30}
+              className="w-[30px] h-[30px] rounded-full cursor-pointer"
+              style={{
+                border: activeItem === 3 ? "2px solid #37a39a" : "none",
+              }}
+            />
+          </Link>
+        ) : (
+          <HiOutlineUserCircle
             size={25}
             className=" 800px:block cursor-pointer dark:text-white text-black"
             onClick={() => setOpen(true)}
-           /> 
-          )
-        }
+          />
+        )}
         <ThemeSwitcher />
       </Menu>
-      {
-        route === "Login" && (
-          <>
-            {
-              open && (
-                <CustomModal 
-                  open={open}
-                  setOpen={setOpen}
-                  setRoute={setRoute}
-                  activeItem={activeItem}
-                  component={Login}
-                />
-              )
-            }
-          </>
-        )
-      }
-      {
-        route === "Sign-Up" && (
-          <>
-            {
-              open && (
-                <CustomModal 
-                  open={open}
-                  setOpen={setOpen}
-                  setRoute={setRoute}
-                  activeItem={activeItem}
-                  component={SignUp}
-                />
-              )
-            }
-          </>
-        )
-      }
-      {
-        route === "Verification" && (
-          <>
-            {
-              open && (
-                <CustomModal 
-                  open={open}
-                  setOpen={setOpen}
-                  setRoute={setRoute}
-                  activeItem={activeItem}
-                  component={Verification}
-                />
-              )
-            }
-          </>
-        )
-      }
+      {route === "Login" && (
+        <>
+          {open && (
+            <CustomModal
+              open={open}
+              setOpen={setOpen}
+              setRoute={setRoute}
+              activeItem={activeItem}
+              component={Login}
+            />
+          )}
+        </>
+      )}
+      {route === "Sign-Up" && (
+        <>
+          {open && (
+            <CustomModal
+              open={open}
+              setOpen={setOpen}
+              setRoute={setRoute}
+              activeItem={activeItem}
+              component={SignUp}
+            />
+          )}
+        </>
+      )}
+      {route === "Verification" && (
+        <>
+          {open && (
+            <CustomModal
+              open={open}
+              setOpen={setOpen}
+              setRoute={setRoute}
+              activeItem={activeItem}
+              component={Verification}
+            />
+          )}
+        </>
+      )}
     </div>
   );
 };
--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,41 +1,40 @@
-'use client'
-import React,{FC, useState} from 'react'
-import Protected from '../hooks/useProtected'
-import Heading from '../utils/Heading'
-import Navbar from '../components/Navbar'
+"use client";
+import React, { FC, useState } from "react";
+import Protected from "../hooks/useProtected";
+import Heading from "../utils/Heading";
+import Navbar from "../components/Navbar";
 import Profile from "../components/Profile/Profile";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 
-type Props = {}
+type Props = {};
 
-const page:FC<Props>= (props: Props) => {
+const page: FC<Props> = (props: Props) => {
   const [open, setOpen] = useState(false);
-  const [activeItem,setActiveItem] = useState(0);
+  const [activeItem, setActiveItem] = useState(3);
   const [route, setRoute] = useState("Login");
-  const {user} = useSelector((state:any) => state.auth)
+  const { user } = useSelector((state: any) => state.auth);
 
   return (
     <div>
-     <Protected>   {/* checking if the user is authenticated before redirecting to the profile page*/}
-     <Heading
-        title={`${user?.name} profile`}
-        description='Eduception is a innovative learning platform for students to lern and get help from their course mentors'
-        keywords='Programming, MERN, React, Nextjs, Maching Lerning/AI'
-      />
-      <Navbar
-        open ={open}
-        setOpen={setOpen}
-        activeItem={activeItem}
-        setRoute={setRoute}
-        route={route}
-      />
-      <Profile 
-        user={user}
-      />
-
-     </Protected>
+      <Protected>
+        {" "}
+        {/* checking if the user is authenticated before redirecting to the profile page*/}
+        <Heading
+          title={`${user?.name} profile`}
+          description="Eduception is a innovative learning platform for students to lern and get help from their course mentors"
+          keywords="Programming, MERN, React, Nextjs, Maching Lerning/AI"
+        />
+        <Navbar
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+        <Profile user={user} />
+      </Protected>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
