dd8bcfb6ffdc90484353b51e179b59f40335b553
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-04-11 02:17:05 +0530

    update changepassword

--- client/app/components/Profile/Profile.tsx
+++ client/app/components/Profile/Profile.tsx
@@ -1,19 +1,19 @@
-'use client'
-import React,{FC, useState} from 'react'
-import SideBarProfile from './SideBarProfile'
-import { useLogOutQuery } from '@/redux/features/auth/authApi';
-import { signOut } from 'next-auth/react';
-import { toast } from 'react-hot-toast';
-import ProfileInfo from '../../components/Profile/ProfileInfo';
+"use client";
+import React, { FC, useState } from "react";
+import SideBarProfile from "./SideBarProfile";
+import { useLogOutQuery } from "@/redux/features/auth/authApi";
+import { signOut } from "next-auth/react";
+import { toast } from "react-hot-toast";
+import ProfileInfo from "../../components/Profile/ProfileInfo";
+import ChangePassword from "./ChangePassword";
 
 type Props = {
-  user:any;
-}
+  user: any;
+};
 
-const Profile :FC<Props>= ({user}) => {
-
+const Profile: FC<Props> = ({ user }) => {
   const [scroll, setScroll] = useState(false);
-  const [avatar, setAvatar] = useState(null); 
+  const [avatar, setAvatar] = useState(null);
   const [active, setActive] = useState(1);
   const [logout, setLogout] = useState(false);
   const {} = useLogOutQuery(undefined, {
@@ -21,28 +21,29 @@ const Profile :FC<Props>= ({user}) => {
   });
   // const router = useRouter();
 
-
   const logOutHandler = async () => {
     setLogout(true);
     await signOut();
-  }
+  };
 
-  if(typeof window !== 'undefined'){
-    window.addEventListener('scroll', () => {
-      if(window.scrollY > 85){
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 85) {
         setScroll(true);
       } else {
         setScroll(false);
       }
-    })
+    });
   }
 
   return (
-    <div className='w-[85%] flex mx:auto'>
-      <div className={`w-[60px] 800px:w-[310px] h-[450px] dark:bg-slate-900  bg-opacity-90 border bg-white dark:border-[#ffffff1d] border-[#00000053] rounded-[5px] shadow-xl dark:shadow-sm mt-[80px] mb-[80px] sticky ${
-        scroll ? 'top-[120px]':'top-[30px]' 
-      } left-[30px]`}>
-        <SideBarProfile 
+    <div className="w-[85%] flex mx:auto">
+      <div
+        className={`w-[60px] 800px:w-[310px] h-[450px] dark:bg-slate-900  bg-opacity-90 border bg-white dark:border-[#ffffff1d] border-[#00000053] rounded-[5px] shadow-xl dark:shadow-sm mt-[80px] mb-[80px] sticky ${
+          scroll ? "top-[120px]" : "top-[30px]"
+        } left-[30px]`}
+      >
+        <SideBarProfile
           user={user}
           active={active}
           avatar={avatar}
@@ -50,20 +51,18 @@ const Profile :FC<Props>= ({user}) => {
           logOutHandler={logOutHandler}
         />
       </div>
-        {
-          active ===1 && (
-            <div className='w-full h-full bg-transparent mt-[80px]'>
-              <ProfileInfo 
-                avatar={avatar}
-                user={user}
-              />
-
-            </div>
-          )
-        }
-
+      {active === 1 && (
+        <div className="w-full h-full bg-transparent mt-[80px]">
+          <ProfileInfo avatar={avatar} user={user} />
+        </div>
+      )}
+      {active === 2 && (
+        <div className="w-full h-full bg-transparent mt-[80px]">
+          <ChangePassword />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
